97f74af405251a09137b95ed76527061e23f8e67
iamzubin <zchoudhary.10@gmail.com>
2022-12-05 16:43:20 +0530

    change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images : {
+    unoptimized : true
+  },
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/links": { page: "/links" },
+    };
+  },
+  assetPrefix: !debug
+    ? "https://anotherplanet-io.github.io/Next-React-Components/"
+    : "",
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "/Resume_iamzubin.pdf",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
---
+++ pages/links.js
@@ -0,0 +1,82 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const links = [
+    {
+      name: "Resume",
+      url: "/Resume_iamzubin.pdf",
+      description: "Checkout my resume here!!!",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/iamzubin/",
+      description: "Find me on linkdin",
+    },
+    {
+      name: "GitHub",
+      url: "https://www.github.com/iamzubin/",
+      description: "Take a look at my open source projects",
+    },
+    {
+      name: "Twitter",
+      url: "https://www.twitter.com/iamzubin/",
+      description: "Follow me on twitter shitposting",
+    },
+    {
+      name: "Email",
+      url: "mailto:zchoudhary.10@gmail.com",
+      description: "Send me an email",
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>iamzub.in | links</title>
+        <meta name="description" content="links to socials and others" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div
+          className={styles.flexRow}
+          style={{
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexWrap: "wrap",
+            width: "100%",
+          }}
+        >
+          <a className={styles.title} href="/" rel="noopener noreferrer">
+            &larr;
+          </a>
+          <h1 className={styles.title}>Links</h1>
+          <h1
+            className={styles.title}
+            style={{
+              color: "transparent",
+              textShadow: "none",
+            }}
+          >
+            &rarr;
+          </h1>
+        </div>
+        <div className={styles.column}>
+          {links.map((link) => (
+            <a
+              href={link.url}
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2>{link.name} &rarr;</h2>
+              <p>{link.description}</p>
+            </a>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
