10e19204037442f240aa9c2806d484e38ebafc20
zubin <8392856+iamzubin@users.noreply.github.com>
2025-05-02 03:09:05 +0530

    refactor: remove old configuration and pages, update project structure and dependencies

---
+++ app/header.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { TextEffect } from '@/components/ui/text-effect'
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <header className="mb-8 flex items-center justify-between">
+      <div>
+        <Link href="/" className="font-medium text-black dark:text-white">
+          Zubin Choudhary
+        </Link>
+        <TextEffect
+          as="p"
+          preset="fade"
+          per="char"
+          className="text-zinc-600 dark:text-zinc-500"
+          delay={0.5}
+        >
+          Full-Stack Developer · Blockchain Engineer
+        </TextEffect>
+      </div>
+    </header>
+  )
+}
