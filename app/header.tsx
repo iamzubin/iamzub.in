a9714e2f94c92b7149cc1c12efb4ca3005e61c71
Zubin c <zchoudhary.10@gmail.com>
2025-06-24 06:01:37 +0530

    Full-stack -> freelance

--- app/header.tsx
+++ app/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          Full-Stack Developer
+          Freelance Developer
         </TextEffect>
       </div>
     </header>
